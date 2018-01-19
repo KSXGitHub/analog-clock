3fa6d509f83e2b760ae56206d8d4b19ec7bcc157
khai96_ <hvksmr1996@gmail.com>
2018-01-19 16:20:28 +0700

    Updates

---
+++ _next/e78430a8-4dcf-4497-8190-79e0738385e8/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[286]);
+            return { page: comp.default }
+          })
+        
